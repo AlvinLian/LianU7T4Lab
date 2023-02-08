56693cd7aa3c43cd2b5baa1fbcf45e42f5053a33
Alvin <alvinl70@nycstudents.net>
2023-02-07 20:06:12 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Main {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> stringList7 = new ArrayList<String>(Arrays.asList("hello", "my", "best", "friend"));
+        ArrayListAlgorithms.duplicateUpperEnd(stringList7);
+        System.out.println(stringList7);
+
+
+
+    }
 }
